a7cca403f7576c303bf07663a4b86c135633f90e
joonsson <joonsson@gmail.com>
2017-10-05 09:27:51 +0200

    Started player

--- src/se/academy/main/Player.java
+++ src/se/academy/main/Player.java
@@ -1,4 +1,29 @@
 package se.academy.main;
 
 public class Player {
+    private int x;
+    private int y;
+    private int lives;
+
+    public Player() {
+        x = 400;
+        y = 400;
+        lives = 1;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getLives() {
+        return lives;
+    }
+    public void move(int dx, int dy) {
+        x += dx;
+        y += dy;
+    }
 }
